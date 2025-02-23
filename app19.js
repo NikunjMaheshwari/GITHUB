c26bc7d40959b5c539e8f5f00a069d5613acbabd
Nikunj Maheshwari <maheshwarinikk333@gmail.com>
2025-02-23 14:01:35 +0530

    new change

--- app19.js
+++ app19.js
@@ -66,24 +66,30 @@
 //git branches(to smooth development process and maintain hierarchy)
 //branch commands - git branch(to check branch)
 //git branch -M main(to rename branch)
-//git checkout <-branch name->(to navigate)
+//git checkout <-branch name->(to navigate)(ek branch se dusri branch jana)
 //git checkout -b <-new branch name->(to create new branch)
-//git branch -d<-branch name->(to delete branch) 
-//to merge branch(git push --set-upstream origin feature)(feature is a branch that we have created.)
+//git branch -d <-branch name->(to delete branch) 
+//to merge branch or to push code of feature branch(git push --set-upstream origin feature)(feature is a branch that we have created.)
 
 //merging code-> git diff <-branch name->(to compare commits,branches,files & more)
-//git merge <-branch name->(to merge 2 branches) or create a PR(pull request)
-//pull request->It lets you tell others changes you've pusheed to a branch in a repository on Github
+//git merge <-branch name->(to merge 2 branches)(ye do command diff and merge to merge from terminal ya git se pull request se) or create a PR(pull request)
+//pull request->It lets you tell others changes you've pushed to a branch in a repository on Github(pull request option comes itself on github by creating a new branch)
 //all files of feature branch will also show in main branch also.
-//pull command//git pull origin main(used to fetch and download content from a remote repo and immediately update the local repo to match that content)
+//pull command(git pull origin main)(used to fetch and download content from a remote repo and immediately update the local repo to match that content)
+//jo new file banai feature branch mai thi khali unko main mai lane k liye vs code mai github pr to aagayi thi pull request se(or merge in vs code)
+
 //merge conflicts(An event that take place when Git is unable to automatically resolve differences in code between two commits)
+//conflict jab aate h same file mai dono branch mai change krdia to git ko samajh nahi aayega ki konsa commit lena or konsa change krna h.
 
 //fixing mistakes -
-//case1:staged changes 
+//case1:staged changes (if unnecessary change add command)
 //git reset <-file name->
-//git reset
-//case 2: commited changes (for one commit)
+//git reset (for all files)
+//case 2: commited changes (for one commit)(agar galti se commit kar diya)
 //git reset HEAD~1
 //case 3:commited changes (for many commits)
 //git reset <-commit hash->
 //git reset --hard <-commit hash->
+
+//forking :-A fork is a new repository that shares code and visibility settings with original "upstream" repository.
+//fork is a rough copy
